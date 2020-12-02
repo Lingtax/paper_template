d7d97df4a2cb360747be942264760484adc1db14
Lingtax <m.ling@deakin.edu.au>
2020-12-02 11:28:16 +1100

    Customise to my defaults

--- R/analysis.R
+++ R/analysis.R
@@ -1,12 +1,16 @@
 # Analysis Script
 
 # Load packages -----------------------------------------------------------
-
+library(tidyverse)
+library(misinformation)
+library(here)
 
 
 # Read data ---------------------------------------------------------------
+df <- read_csv(here::here("data", ".csv"))
+meta <- read_csv(here::here("data", ".csv"))
 
-
+df <-  df %>% meta_rename(meta, old = name_raw, new = name_clean)
 
 # Clean Data --------------------------------------------------------------
 
